19ec758911b4a9a6a34253a37ac820d565f317f5
Nicholas_Huang713 <nhuang713@gmail.com>
2020-10-26 11:47:07 -0700

    navbar input updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from './components/Layout/Layout';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home/Home';
+import Following from './components/Following/Following';
 import './App.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <Layout>
         <Switch>
           <Route exact path="/" component={Home} />
-
+          <Route exact path="/following" component={Following} />
         </Switch>
       </Layout>
     </Router>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
 import './NavBar.css';
 
 export default function NavBar() {
+    const [inputText, setInputText] = useState('');
+
+    const handleKeyUp = (e) => {
+        e.preventDefault();
+        if(e.key === 'Enter') {
+            console.log('ENTER PRESSED')
+            console.log(inputText);
+            e.preventDefault();
+            // const userId = 1;
+            // const title = inputText;
+            // const completed = false;
+            // const newTodo = new Todo(userId, title, completed);
+            // dispatch(createTodo(newTodo));
+            setInputText('');
+        }
+    };
+
+
     return (
         <div className="nav-wrapper">
-            <div><h1>GitHub Search</h1></div>
+            <div><h2>GitHub Search</h2></div>
             <div>
-                Home
+                <Link to="/">Home</Link>
             </div>
             <div>
-                Following
+                <Link to="/following">Following</Link>
+            </div>
+            <div>
+                <input 
+                    className="nav-input" 
+                    type="text"
+                    placeholder="Add a Todo" 
+                    value={inputText}
+                    onChange={e => setInputText(e.target.value)}
+                    onKeyUp={handleKeyUp}
+                />
+                <Button variant="contained"><SearchIcon /></Button>
             </div>
         </div>
     )

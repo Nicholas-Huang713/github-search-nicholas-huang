97d6bc984b076b75e604eab578087247a898e231
Nicholas_Huang713 <nhuang713@gmail.com>
2020-10-26 11:57:37 -0700

    github apis added

--- src/api/githubApi.js
+++ src/api/githubApi.js
@@ -1,1 +1,29 @@
 import axios from 'axios';
+
+export const getUser = (userName) => {
+    axios.get(`https://api.github.com/users/${userName}`, {
+        auth: {
+         username: 'nicholas-huang713',
+         password: 'b730dd7e9ea9e57dc0d0ca79654767848de66f13'
+        },
+    })
+    .then((res) => {
+        console.log(res.data);
+        res.json();
+    })
+    .catch(err => console.log(err));
+}
+
+export const getUserFollowing = (userName, pageIndex, pageSize) => {
+    axios(`https://api.github.com/users/${userName}/following?page=${pageIndex}&per_page=${pageSize}`, {
+        auth: {
+            username: 'nicholas-huang713',
+            password: 'b730dd7e9ea9e57dc0d0ca79654767848de66f13'
+        },
+    })
+    .then((res) => {
+        console.log(res.data)
+        res.json();
+    })
+    .catch(err => console.log(err));
+}
